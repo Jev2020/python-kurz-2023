96ad499f67eff6474cf6e8f034765f03810e4e13
Jevhenij Kuzmenko <kjevhenij@gmail.com>
2023-03-31 14:25:09 +0200

    Ukol X

---
+++ Ukol2-x.py
@@ -0,0 +1,12 @@
+jmeno_a_prijmeni = input('Zadej jmeno a prijmeni:')
+print(jmeno_a_prijmeni.upper())
+
+jmeno_a_prijmeni = input('Zadej jmeno a prijmeni:')
+print(jmeno_a_prijmeni.lower())
+
+jmeno_a_prijmeni = input('Zadej jmeno a prijmeni:')
+print(jmeno_a_prijmeni.title())
+
+jmeno_a_prijmeni = input('Zadej jmeno a prijmeni:')
+Jmeno_a_Prijmeni = jmeno_a_prijmeni.split(' ')
+print(f'{Jmeno_a_Prijmeni[0][0:1]}.{Jmeno_a_Prijmeni[1][0:1]}.')
